9c494acd7a58b92757b5eaa5513a31835c26c710
Ian Henderson <ianwhen@gmail.com>
2014-05-20 01:17:29 -0700

    Moved jsmess files to client root due to problems loading game file

--- client/messloader.js
+++ client/messloader.js
@@ -96,7 +96,7 @@ var update_countdown = function() {
     var headID = document.getElementsByTagName("head")[0];
 		var newScript = document.createElement('script');
 		newScript.type = 'text/javascript';
-		newScript.src = 'messmegadriv.js';
+		newScript.src = 'genesis/messmegadriv.js';
 		headID.appendChild(newScript);
   }
 };
--- server.js
+++ server.js
@@ -1,26 +1,8 @@
-var express = require('express'),
-    routes = require('./routes'),
-    api = require('./routes/api');
+var express = require('express');
+var app = express();
+var port = 3000;
 
-var port = process.env.PORT || 4568;
-var app = module.exports = express.createServer();
-
-app.configure(function() {
-    app.use(express.bodyParser());
-    // app.use(express.methodOverride());
-    app.use(express.static(__dirname + '/client'));
-    app.use(app.router);
-});
-
-//RESTful Routes
-// app.get('/api/posts', api.posts);
-// app.delete('/api/post/:post_id', api.postDelete);
-
-app.get('*', routes.index);
-
-// var app = require('./server-config.js');
-
-
-app.listen(port);
-
+app.use(express.static(__dirname + '/client'));
+app.listen(process.env.PORT || port);
 console.log('Server now listening on port ' + port);
+
