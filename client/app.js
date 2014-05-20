f79bffa15a4d0da577108c420d1cf985bbdb1b87
Ian Henderson <ianwhen@gmail.com>
2014-05-20 15:05:30 -0700

    Cleaned up directory, added popup info menu

--- client/app.js
+++ client/app.js
@@ -1,20 +1,7 @@
-var messapp = angular.module('messapp', ['ngRoute'])
-  .config(function($routeProvider, $locationProvider){
-    $routeProvider
-      .when('/', {
-        templateUrl: 'index.html',
-        controller: ''
-      })
-      .when('/dd', {
-        templateUrl: 'genesis.html',
-        controller: ''
-      })
-      .when('/sor', {
-        templateUrl: 'genesis.html',
-        controller: ''
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-    $locationProvider.html5mode(true);
+var messapp = angular.module('messapp', [])
+  .controller('infotoggle', function($scope){
+    $scope.showinfo = false;
+    $scope.toggleinfo = function(){
+      this.showinfo = !this.showinfo;
+    };
   });
