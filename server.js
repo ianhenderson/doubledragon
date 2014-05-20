bcd55827c0be875c668ac72c2d2a6c74ccc025f3
Ian Henderson <ianwhen@gmail.com>
2014-05-20 15:49:20 -0700

    Added deploy script, compression for express server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var compress = require('compression');
 var app = express();
 var port = 3000;
 
+app.use(compress({ threshold: 512}));
 app.use(express.static(__dirname + '/client'));
 app.listen(process.env.PORT || port);
 console.log('Server now listening on port ' + port);
